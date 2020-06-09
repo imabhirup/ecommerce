b7cf9dc58ae4adaa6c3c2700bb6d4813113b3c2c
@manassantra <manas.nshmdgp@gmail.com>
2020-06-09 22:46:48 +0530

    navbar first design

--- b2c/b2c-react/src/App.js
+++ b2c/b2c-react/src/App.js
@@ -1,25 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../src/logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome-4.7.0/css/font-awesome.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar1">
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <a className="brand" href="index.html">
+              <i className="fa fa-shopping-cart fa-2x"></i>
+              &nbsp;
+              AMEZSTORE
+            </a>
+          </div>
+          <div className="col-10">
+            <div className="row">
+              <div className="col-7">
+                <from className="form-inline">
+                  <input className="form-control" id="inputfld" type="search" aria-label="Search" placeholder="Search categories or products" />
+                  <button className="btn btn-outline-success">
+                    <i className="fa fa-search"></i>
+                  </button>
+                </from>
+              </div>
+              <div className="col-3 nav-item borderXwidth">
+                <a className="nav-link" href="login.html">LOGIN | SIGNUP</a>
+              </div>
+              <div className="col-2 nav-item borderXwidth">
+                <a className="nav-link" href="faq.html">FAQ</a>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-2 nav-item borderXwidth">
+                <a className="nav-link" href="faq.html">ALL PRODUCTS</a>
+              </div>
+              <div className="col-1 nav-item borderXwidth">
+                <a className="nav-link" href="faq.html">MEN</a>
+              </div>
+              <div className="col-2 nav-item borderXwidth">
+                <a className="nav-link" href="faq.html">WOMEN</a>
+              </div>
+              <div className="col-1 nav-item borderXwidth">
+                <a className="nav-link" href="faq.html">KIDS</a>
+              </div>
+              <div className="col-3 nav-item borderXwidth">
+                <a className="nav-link" href="faq.html">WORLD CRAFTS</a>
+              </div>
+              <div className="col-2 nav-item borderXwidth">
+                <a className="nav-link" href="faq.html">NEW ARRIVALS</a>
+              </div>
+              <div className="col-1 nav-item borderXwidth">
+                <a className="nav-link" href="faq.html">
+                  <i className="fa fa-shopping-cart"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
   );
 }
 
